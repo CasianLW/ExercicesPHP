33292a3d3bea32431371beebf1593444fc1c34a5
Casian <ciorbcasian@gmail.com>
2022-11-24 12:38:24 +0100

    Correction des liens menu pour l'environnement localhost et fonctionnel dans sandbox

--- PHP_Basics/menu.php
+++ PHP_Basics/menu.php
@@ -3,7 +3,34 @@
 
 
 <?php require_once 'fonctionsMenu.php'; 
-$url1 =  '/PHP_Basics'  ?>
+  if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') 
+  $url = "https"; 
+else
+  $url = "http"; 
+  
+// Ajoutez // à l'URL.
+$url .= "://"; 
+  
+// Ajoutez l'hôte (nom de domaine, ip) à l'URL.
+$url .= $_SERVER['HTTP_HOST']; 
+  
+// Ajouter l'emplacement de la ressource demandée à l'URL
+$url .= $_SERVER['REQUEST_URI']; 
+
+$uri_path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uri_segments = explode('/', $uri_path);
+
+echo $uri_segments[1]; // for www.example.com/user/account you will get 'user'
+    
+if($uri_segments[1] === "PHP_Basics"){
+    $url1 =  '/ExercicesPHP/PHP_Basics';
+}else{
+    $url1 =  ''; 
+}
+// Afficher l'URL
+// echo $url; 
+// var_dump($uri_segments[2]);
+?>
 <?= nav_item("$url1/index.php",'Accueil', $class);?>
 <?= nav_item("$url1/contact.php",'Contact', $class);?>
 <?= nav_item("$url1/jeu.php",'Jeu GET/POST', $class);?>
