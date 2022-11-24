89b4b30193efbd214bd65b73ea2f58711200c690
Casian <ciorbcasian@gmail.com>
2022-11-24 12:19:21 +0100

    Correction des liens du le menu

--- PHP_Basics/menu.php
+++ PHP_Basics/menu.php
@@ -2,16 +2,17 @@
 
 
 
-<?php require_once 'fonctionsMenu.php'; ?>
-<?= nav_item('/index.php','Accueil', $class);?>
-<?= nav_item('/contact.php','Contact', $class);?>
-<?= nav_item('/jeu.php','Jeu GET/POST', $class);?>
-<?= nav_item('/jeu-post.php','Composer une glace', $class);?>
-<?= nav_item('/menu-list.php','Liste Menu', $class);?>
-<?= nav_item('/newsletter.php','Newsletter', $class);?>
-<?= nav_item('/profil.php','Profil', $class);?>
-<?= nav_item('/profil_tableau.php','Vérification +18', $class);?>
-<?= nav_item('/dashboard.php','Dashboard', $class);?>
+<?php require_once 'fonctionsMenu.php'; 
+$url1 =  '/PHP_Basics'  ?>
+<?= nav_item("$url1/index.php",'Accueil', $class);?>
+<?= nav_item("$url1/contact.php",'Contact', $class);?>
+<?= nav_item("$url1/jeu.php",'Jeu GET/POST', $class);?>
+<?= nav_item("$url1/jeu-post.php",'Composer une glace', $class);?>
+<?= nav_item("$url1/menu-list.php",'Liste Menu', $class);?>
+<?= nav_item("$url1/newsletter.php",'Newsletter', $class);?>
+<?= nav_item("$url1/profil.php",'Profil', $class);?>
+<?= nav_item("$url1/profil_tableau.php",'Vérification +18', $class);?>
+<?= nav_item("$url1/dashboard.php",'Dashboard', $class);?>
 
 
 
