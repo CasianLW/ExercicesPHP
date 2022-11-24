3ad39f72fd4410a2abdf46d78f0cc07ba00d2c17
Casian <ciorbcasian@gmail.com>
2022-11-24 12:16:21 +0100

    remplacement du pdo mysqlite par le pdo reel de aws ainsi que les modification necessaires a son bon fonctionnement

--- TP_LivreOr_Meteo_Blog/blog/edit.php
+++ TP_LivreOr_Meteo_Blog/blog/edit.php
@@ -1,8 +1,34 @@
 <?php 
-$pdo = new PDO('sqlite:../data.db', null, null, [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
-]);
+require_once '../vendor/autoload.php';
+// $dotenv = 
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+// $_ENV['S3_BUCKET'];
+// pdo classique aws
+$hostname = $_ENV['DB_HOST'];
+$dbname = $_ENV['DB_NAME'];
+$user = $_ENV['DB_USER'];
+$pass = $_ENV['DB_PASSWORD'];
+$port = $_ENV['DB_PORT'];
+try {
+    $pdo = new PDO("mysql:host=$hostname;dbname=$dbname;port=$port", $user, $pass);
+    // foreach($pdo->query('SELECT * from FOO') as $row) {
+    //     print_r($row);
+    // }
+    // $pdo = null;
+    // var_dump("ca fonctionne" . $dbname);
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+
+// pdo mysqlite
+// $pdo = new PDO('sqlite:../data.db', null, null, [
+//     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+//     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
+// ]);
 // $pdo->setAttribute(PDO::ATTR_ERRMODE,  PDO::ERRMODE_EXCEPTION);
 $error = null;
 $success = null;
@@ -47,13 +73,13 @@ require '../elements/header.php'; ?>
 
     <?php else: ?>
    
-            
+            <!-- <?= var_dump($post) ?> -->
                 <form action="" method="post">
                     <div class="form-group">
-                        <input type="text" class="form-control" name="name" value="<?=htmlentities($post->name)?>">
+                        <input type="text" class="form-control" name="name" value="<?=htmlentities($post['name'])?>">
                     </div>
                     <div class="form-group">
-                        <textarea type="text" class="form-control" name="content" ><?=htmlentities($post->content)?></textarea>
+                        <textarea type="text" class="form-control" name="content" ><?=htmlentities($post['content'])?></textarea>
                     </div>
                     <button class="btn btn-primary">Sauvegarder</button>
                 </form>
